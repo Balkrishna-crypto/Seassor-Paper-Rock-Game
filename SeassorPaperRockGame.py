4a2651784b132920410a6989140dd370d32b0876
Peter <balkrishna.oli@tekvortex.local>
2022-04-06 10:31:18 +0545

    Code minimized

--- SeassorPaperRockGame.py
+++ SeassorPaperRockGame.py
@@ -1,34 +1,42 @@
 import random
-randNo= random.randint(1,3)
+
+CHOICE='spr'
+
+#defining the funtion to chose s p r with the help of random function
+def get_computer_choice():
+    
+    Comp_option = random.randint(0, 2)
+    Comp_option = CHOICE[Comp_option]
+    return Comp_option
+
+
+Comp_option=get_computer_choice()
 player =input("Enter Seassor 's' or Paper 'p' or Rock 'r' :")
-if randNo==1:
-    comp='s'
-elif randNo==2:
-    comp='p'
-elif randNo==3:
-    comp='r'
-print(f"your input for Seassor 's' or Paper 'p' or Rock 'r'     is   :{player}")
-print(f"Computer input for Seassor 's' or Paper 'p' or Rock 'r' is   :{comp} ")
-def gameWin(player,comp):
-    if comp==player:
+print(f"Your input for Seassor 's' or Paper 'p' or Rock 'r'     is   :{player}")
+print(f"Computer input for Seassor 's' or Paper 'p' or Rock 'r' is   :{Comp_option} ")
+
+
+player=player.lower()
+def gameWin(player,Comp_option):
+    if Comp_option==player:
         return None
-    elif comp=='s':
+    elif Comp_option=='s':
         if player=='p':
             return False
         else:
             return True
-    elif comp=='p':
+    elif Comp_option=='p':
         if player=='r':
             return False
         else:
             return True
-    elif comp=='r':
+    elif Comp_option=='r':
         if player=='s':
             return False
         else :
             return True
 
-a=gameWin(player,comp)
+a=gameWin(player,Comp_option)
 if a==None:
     print("Game is tie!")
 elif a:
